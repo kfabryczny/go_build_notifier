ad431057ca684ce17aac3006144c0ed2a100724c
Klaudiusz Fabryczny <klodus@gmail.com>
2017-05-06 00:07:56 +0200

    In progress

--- gbn/main.go
+++ gbn/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	//"fmt"
+	"log"
+	"net/http"
+)
+
+const (
+	gitURL    = "https://github.com"
+	gitAPIURL = "https://api.github.com"
+	gbnURL    = "/notifications/mail/"
+	gbnPort   = ":10005"
+)
+
+func init() {
+	log.SetFlags(0)
+}
 
 func main() {
-	fmt.Println("Hello Gophers !!!")
+	http.HandleFunc(gbnURL, handler)
+	http.ListenAndServe(gbnPort, nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
 }
